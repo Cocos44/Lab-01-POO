eb1ea737e8bbbc02dddcbe0fdf01b0ba53002e92
Vlad Andrei Grigoras <vladandreigrigoras@gmail.com>
2024-10-17 13:37:59 +0300

    Correct final commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -28,13 +28,13 @@ public class HelloWorld {
          * Exercitiu 3
          */
         // System.out.println(yes == no || grade > amount);
-        // System.out.println(amount == 40.0 || 50.0;);
+        System.out.println(amount == 40.0 || amount == 50.0);
         // System.out.println(hiVal != loVal || loVal < 0);
         // System.out.println(true || hello.lenght() > 0);
         // System.out.println(hello.isEmpty() && yes);
         // System.out.println(grade <= 100 && !false);
         // System.out.println(!yes || no);
-        // System.out.println(grade > 75 > amount);
+        System.out.println(grade > 75 && amount < 75);
         // System.out.println(amount <= hiVal && amount >= loVal);
         // System.out.println(no && !no || yes && !yes);
 
@@ -42,27 +42,27 @@ public class HelloWorld {
          * Exercitiu 4
          */
 
-        // float a = 100.25f;
-        // long b = (long)a;
+         // float a = 100.25f;
+         // long b = (long)a;
 
-        // System.out.println("value of a: " + a);
-        // System.out.println("value of b: " + b);
-        // int c = (int)b;
-        // System.out.println("value of c: " + c);
-        // byte d = (byte)c;
-        // System.out.println("value of d: " + d);
+         // System.out.println("value of a: " + a);
+         // System.out.println("value of b: " + b);
+         // int c = (int)b;
+         // System.out.println("value of c: " + c);
+         // byte d = (byte)c;
+         // System.out.println("value of d: " + d);
 
         /**
          * Exercitiu 5
          */
 
-        int a = 5;
-        System.out.println(a + -a -a++ % 10);
-        System.out.println(a - a + --a / 10);
-        for (int i = 2; i < 5;) {
-            i++;
-            a += a;
-        }
-        System.out.println("a = " + a);
+        // int a = 5;
+        // System.out.println(a + -a -a++ % 10);
+        // System.out.println(a - a + --a / 10);
+        // for (int i = 2; i < 5;) {
+        //     i++;
+        //     a += a;
+        // }
+        // System.out.println("a = " + a);
     }
 }
